3d7e7ef6a50533a98d222894a39de90c9831fb8e
Taejun Park <ekswn120@gmail.com>
2025-01-23 19:57:20 +0900

    git init

---
+++ src/main/java/org/jnjeaaaat/snms/SnmsApplication.java
@@ -0,0 +1,13 @@
+package org.jnjeaaaat.snms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jnjeaaaat/snms/SnmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jnjeaaaat.snms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
